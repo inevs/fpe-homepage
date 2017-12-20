29b92a949c4247392cbb206b8d5ade629de450b5
Markus Schwarz <post@markus-schwarz.net>
2017-12-20 14:36:20 +0100

    adds script to start containers

---
+++ start-containers.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo "Starting database and web server"
+docker-compose up -d
+
+echo "Waiting until containers are ready"
+sleep 2
+
+echo "Importing database dump"
+
+for ID in $(docker ps --filter='name=fpehomepage_db' -q)
+  do
+		docker exec $ID gunzip -c /sources/dump.sql.gz | mysql -uroot -pexample -h127.0.0.1
+  done
+
+echo "Done"
