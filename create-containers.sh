a11653cf46cad11b3c57407666278484eba70e67
Markus Schwarz <post@markus-schwarz.net>
2017-12-20 17:37:27 +0100

    removes old containers and fixes permissions

--- create-containers.sh
+++ create-containers.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
-echo "Starting database and web server"
+echo "###### Destroying all old containers ######"
+docker-compose down
+
+echo "###### Creating containers for database and web server ######"
 docker-compose up -d
 
-echo "Waiting until containers are ready"
+echo "###### Waiting until containers are ready ######"
 sleep 1
 ID=$(docker ps --filter='name=fpehomepage_db' -q)
 while : ; do
 	docker exec $ID mysql -uroot -pexample -h127.0.0.1 -e 'show databases;' &> /dev/null
 	[[ $? -gt 0 ]] || break
-	echo "still waiting for mysql to finish initialization"
+	echo "###### still waiting for mysql to finish initialization ######"
 	sleep 2
 done
 
-echo "Importing database dump"
+echo "###### Importing database dump ######"
 ./database/import-dump.sh
-echo "Done"
+
+echo "###### Fixing permissions of sites directory to ######"
+sudo chmod a+w -R sites
+
+echo "###### Done ######"
