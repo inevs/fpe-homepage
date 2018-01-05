bb21c5af6ccce6621677138cd012d59cf689956f
Markus Schwarz <post@markus-schwarz.net>
2018-01-05 20:13:11 +0100

    styles news page

--- install.sh
+++ install.sh
@@ -19,9 +19,12 @@ done
 echo "###### Importing database dump ######"
 ./database/import-dump.sh
 
-echo "###### Changing ownership to www-data ######"
+echo "###### Changing ownership in drupal to www-data ######"
 DRUPAL_ID=$(docker ps --filter='name=fpehomepage_drupal' -q)
 docker exec $DRUPAL_ID bash -c 'chown -R www-data:www-data /var/www/html/sites'
+echo "###### Changing ownership in grunt to node ######"
+GRUNT_ID=$(docker ps --filter='name=fpehomepage_grunt' -q)
+docker exec $GRUNT_ID bash -c 'chown -R node:node /data'
 
 echo "###### Fixing file permissions. Please provide your root password to do so. ######"
 sudo chmod -R a+w sites themes modules
