3ad40a8ee4b2d9cda6c45c43016cb86a183bd1d8
Markus Schwarz <post@markus-schwarz.net>
2018-02-19 21:19:36 +0100

    #34: exports and synchronizes configuration

--- update-production.sh
+++ update-production.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 DRUPAL_ID=$(docker ps --filter='name=fpehomepage_drupal' -q)
-docker exec $DRUPAL_ID bash -c 'sitecopy --fetch theme_production && sitecopy --update -k theme_production'
+docker exec $DRUPAL_ID bash -c 'rm -rf /var/www/html/sites/default/files/config_*/sync \
+    && drush config-export \
+    && sitecopy --fetch --allsites \
+    && sitecopy --update -k --allsites'
+
+echo "Go to https://fighting-pirates.net/drupal/admin/config/development/configuration to import the changes"
