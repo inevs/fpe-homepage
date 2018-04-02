70d634f07b10c20b9dd08a5e26760b381e71f303
Sven Günther <sven.guenther@it-agile.de>
2018-04-02 12:36:11 +0200

    adding notes

--- modules/custom/livescore/js/livescore.js
+++ modules/custom/livescore/js/livescore.js
@@ -1,6 +1,6 @@
 (function ($) {
   
-  function updateScore() {
+  function updateLiveScore() {
     console.log("updateScore");
     livescoreEndpoint = "https://footballscores.herokuapp.com/games/" + drupalSettings.livescore.gameId + ".json"
     $.ajax({
@@ -8,24 +8,41 @@
       dataType: "json",
       success: function (data) {
         console.log(data);
-        $(".scores").empty();
-        $(".scores").append(data["score"]["away"]["total"] + " : " + data["score"]["home"]["total"]);
+        updateScores(data["score"]);
+        updateNotes(data["notes"]);
       },
     });  
   };
 
-  updateScore();
-  var autoTimer = setInterval(updateScore, 5000);
+  updateLiveScore();
+  var autoTimer = setInterval(updateLiveScore, 5000);
   $autoreloadButton = $("#autoreload");
   $autoreloadButton.click( function() {
     $autoreloadButton.toggleClass("highlighted");
     if ($autoreloadButton.hasClass("highlighted")) {
       console.log("start auto");
-      autoTimer = setInterval(updateScore, 5000);
+      autoTimer = setInterval(updateLiveScore, 5000);
     } else {
       console.log("stop auto");
       clearInterval(autoTimer);
     }
   });
+
+  function updateScores(data) {
+    $scores = $(".scores");
+    $scores.empty();
+    $scores.append(data["away"]["total"] + " : " + data["home"]["total"]);
+  }
+
+  function updateNotes(data) {
+    $notes = $(".notes");
+    $notes.empty();
+    data.forEach(note => {
+      note_data = $('<div>', {class: 'note'})
+        .append($('<div>', {class: 'timestamp'})).append(note["created_at"])
+        .append($('<div>', {class: 'text'})).append(note["text"]);
+      $notes.append(note_data);
+    });
+  }
 
 })(jQuery);
