3e174dadc72cb61e0339afb100578d721162e08c
Sven Günther <sven.guenther@it-agile.de>
2018-04-02 12:09:50 +0200

    start / stop autotimer

--- modules/custom/livescore/js/livescore.js
+++ modules/custom/livescore/js/livescore.js
@@ -1,13 +1,31 @@
-console.log(drupalSettings.livescore);
-
 (function ($) {
-  livescoreEndpoint = "https://footballscores.herokuapp.com/games/" + drupalSettings.livescore.gameId + ".json"
-  $.ajax({
-    url: livescoreEndpoint,
-    dataType: "json",
-    success: function (data) {
-      console.log(data);
-      $(".scores").append(data["score"]["away"]["total"] + " : " + data["score"]["home"]["total"]);
-    },
+  
+  function updateScore() {
+    console.log("updateScore");
+    livescoreEndpoint = "https://footballscores.herokuapp.com/games/" + drupalSettings.livescore.gameId + ".json"
+    $.ajax({
+      url: livescoreEndpoint,
+      dataType: "json",
+      success: function (data) {
+        console.log(data);
+        $(".scores").empty();
+        $(".scores").append(data["score"]["away"]["total"] + " : " + data["score"]["home"]["total"]);
+      },
+    });  
+  };
+
+  updateScore();
+  var autoTimer = setInterval(updateScore, 5000);
+  $autoreloadButton = $("#autoreload");
+  $autoreloadButton.click( function() {
+    $autoreloadButton.toggleClass("highlighted");
+    if ($autoreloadButton.hasClass("highlighted")) {
+      console.log("start auto");
+      autoTimer = setInterval(updateScore, 5000);
+    } else {
+      console.log("stop auto");
+      clearInterval(autoTimer);
+    }
   });
+
 })(jQuery);
