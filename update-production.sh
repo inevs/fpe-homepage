427664d524cd45724d4d467ff9d53ea1c6c775cf
Sven Günther <sven.guenther@it-agile.de>
2018-05-01 10:53:56 +0200

    move homepage from /drupal to /

--- update-production.sh
+++ update-production.sh
@@ -6,4 +6,4 @@ docker exec $DRUPAL_ID bash -c 'rm -rf /var/www/html/sites/default/files/config_
     && sitecopy --fetch --allsites \
     && sitecopy --update -k --allsites'
 
-echo "Go to https://fighting-pirates.net/drupal/admin/config/development/configuration to import the changes"
+echo "Go to https://fighting-pirates.net/admin/config/development/configuration to import the changes"
