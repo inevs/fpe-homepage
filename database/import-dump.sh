d78a2a564592b4eb1f0195b11dfbcbb5d689dfb2
Sven Günther <sven.guenther@it-agile.de>
2018-08-06 16:32:00 +0200

    update drupal 8.5.6 and some name changes for db machine

--- database/import-dump.sh
+++ database/import-dump.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for ID in $(docker ps --filter='name=fpehomepage_db' -q)
+for ID in $(docker ps --filter='name=fpehomepage_db_1' -q)
   do
 		docker exec $ID bash -c 'gunzip -c /sources/dump.sql.gz | mysql -uroot -pexample -h127.0.0.1'
   done
